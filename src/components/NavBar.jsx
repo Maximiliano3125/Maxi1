c048258de7dc999ef2b9ac4cdaebbc359c8f64f5
Maxi <martitschmaximiliano1112@gmail.com>
2026-01-31 16:15:51 -0300

    CreaTuLanding1+Martitsch

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import CartWidget from './CartWidget'
+
+function NavBar() {
+  return (
+    <nav>
+      <h2>Mi Tienda</h2>
+
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+
+      <CartWidget />
+    </nav>
+  )
+}
+export default NavBar
